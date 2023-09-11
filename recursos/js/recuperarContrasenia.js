a0a31ad07e0b3c64b319c3857892f68f14ae59da
Kevin Rivera <shimpo11.kr@gmail.com>
2023-09-11 10:36:47 -0500

    Logica de la recuperacion de contraseña

---
+++ recursos/js/recuperarContrasenia.js
@@ -0,0 +1,18 @@
+
+// const form = document.getElementById("form");
+// const username = document.getElementById("username");
+
+
+
+// form.addEventListener("submit",function(event){
+//   event.preventDefault();
+//   let users = Array(
+//     {
+//       usuario: username.value,
+//     }
+//   );
+//   console.log(users);
+//   localStorage.setItem("user",JSON.stringify(users));
+//   alert("envio exitoso");
+//   location.href="login.html";
+// });
